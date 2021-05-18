eeedb2ee9436c92f989e73f1bdf3bbb22d111fdc
kamyar gerami <gerami.kamyar@yahoo.com>
2021-05-18 20:04:05 +0430

    add google tag manager

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -12,6 +12,20 @@
     <link rel="icon" href="{{asset('images/favicon.png')}}" type="image/png">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     @yield('style')
+
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5C53TQH');</script>
 </head>
 <body>
 
@@ -21,5 +35,9 @@
 
 <script src="{{asset('js/app.js')}}"></script>
 @yield('script')
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5C53TQH"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 </body>
 </html>
