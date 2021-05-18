e4e502b8ccb7d8845b8a668ebc75472150525eaa
kamyar gerami <gerami.kamyar@yahoo.com>
2021-05-18 21:27:22 +0430

    add pwa

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
 $('.select2').select2({
     width: '100%',
     placeholder: 'چی داری تو خونه؟',
