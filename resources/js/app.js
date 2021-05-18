e9a032099bfde860eafd8da421c1f62ef62dc1cf
kamyar gerami <gerami.kamyar@yahoo.com>
2021-05-18 20:43:59 +0430

    add persian to select2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 require('./bootstrap');
 require('bootstrap');
-window.$ = require('jquery');
+window.$ = window.jQuery = require('jquery');
 require('select2');
+require('select2/dist/js/i18n/fa');
 require('./custom');
--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,4 +1,6 @@
 $('.select2').select2({
     width: '100%',
-    placeholder: 'چی داری تو خونه؟'
+    placeholder: 'چی داری تو خونه؟',
+    maximumSelectionLength: 3,
+    language: "fa"
 });
